62997144a4ce4dc50c4bb434d6814edb6b2f521c
Uday Bhaskhar <uday.pydi@evivehealth.com>
2017-03-02 01:13:36 +0530

    Hunter Project 1

---
+++ DuplicateArrayElements.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class DuplicateArrayElements
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		HashSet<Integer> registerationNumber = new HashSet<Integer>();
+		Scanner scan = new Scanner(System.in);
+		int numberOfInputs = scan.nextInt();
+		for(int i = 0; i < numberOfInputs; i++) {
+			int n = scan.nextInt();
+			if (!registerationNumber.add(n)) {
+				System.out.print(n);
+			}
+		}
+	}
+}
